96896ce951813cd73fd4ca488e9a687ea1c8d5b2
Falconed11 <almacesardavid@gmail.com>
2024-05-07 16:06:27 +0700

    import operasional kantor

--- app/(default)/alat/ui.jsx
+++ app/(default)/alat/ui.jsx
@@ -24,65 +24,28 @@ import Link from "next/link";
 const apiPath = getApiPath();
 
 export default function App() {
-  const [json, setJson] = useState([]);
   const [reportList, setReportList] = useState([]);
   const report = useDisclosure();
-  const handleFileUpload = (jsonData) => {
-    // console.log(jsonData);
-    // Do something with the converted JSON object, e.g., send it to an API
-    jsonData = jsonData.map((v) => {
-      v.tanggal = getDate(excelToJSDate(v.tanggal));
-      return v;
-    });
-    setJson(jsonData);
-    console.log(json);
-  };
-  const handleButtonUploadExcelPress = async () => {
-    if (json.length == 0) return alert("File belum dipilih");
-    setReportList([]);
-    try {
-      const responses = await Promise.all(
-        json.map((v) =>
-          fetch(`${apiPath}produk`, {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-              // 'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            body: JSON.stringify(v),
-          })
-        )
-      );
-      const dataArray = await Promise.all(
-        responses.map((response) => response.json())
-      );
-      setReportList(dataArray.map((v, i) => `${i}. ${v.message}`));
-    } catch (e) {
-      console.error(e);
-    }
-    setJson([]);
-    report.onOpen();
-  };
-
   return (
-    <div>
-      <div className=" border bg-white rounded-lg p-2">
-        <div className="flex gap-2">
-          Omset
-          <div>
-            <Link
-              className="bg-primary text-white p-2 rounded-lg inline-block"
-              href={"/produk.xlsx"}
-            >
-              Download Format
-            </Link>
-          </div>
-          <FileUploader onFileUpload={handleFileUpload} />
-          <Button color="primary" onPress={handleButtonUploadExcelPress}>
-            Upload Excel
-          </Button>
-        </div>
-      </div>
+    <div className="flex flex-col gap-2">
+      <TemplateImport
+        report={report}
+        setReportList={setReportList}
+        name={"Pengeluaran Proyek"}
+        apiendpoint={"importpengeluaranproyek"}
+      />
+      <TemplateImport
+        report={report}
+        setReportList={setReportList}
+        name={"Pembayaran Proyek"}
+        apiendpoint={"importpembayaranproyek"}
+      />
+      <TemplateImport
+        report={report}
+        setReportList={setReportList}
+        name={"Operasional Kantor"}
+        apiendpoint={"importoperasionalkantor"}
+      />
       {/* upload report */}
       <Modal
         isOpen={report.isOpen}
@@ -109,6 +72,68 @@ export default function App() {
           )}
         </ModalContent>
       </Modal>
+    </div>
+  );
+}
+
+function TemplateImport({ setReportList, report, name, apiendpoint }) {
+  const [json, setJson] = useState([]);
+  const handleFileUpload = (jsonData) => {
+    // console.log(jsonData);
+    // Do something with the converted JSON object, e.g., send it to an API
+    jsonData = jsonData.map((v) => {
+      v.tanggal = getDate(excelToJSDate(v.tanggal));
+      return v;
+    });
+    setJson(jsonData);
+    console.log(json);
+  };
+  const handleButtonUploadExcelPress = async (apiendpoint) => {
+    if (json.length == 0) return alert("File belum dipilih");
+    setReportList([]);
+    try {
+      const responses = await Promise.all(
+        json.map((v) =>
+          fetch(`${apiPath}${apiendpoint}`, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: JSON.stringify(v),
+          })
+        )
+      );
+      const dataArray = await Promise.all(
+        responses.map((response) => response.json())
+      );
+      setReportList(dataArray.map((v, i) => `${i}. ${v.message}`));
+    } catch (e) {
+      console.error(e);
+    }
+    setJson([]);
+    report.onOpen();
+  };
+  return (
+    <div className=" border bg-white rounded-lg p-2">
+      {name}
+      <div className="flex gap-2">
+        <div>
+          <Link
+            className="bg-primary text-white p-2 rounded-lg inline-block"
+            href={"/produk.xlsx"}
+          >
+            Download Format
+          </Link>
+        </div>
+        <FileUploader onFileUpload={handleFileUpload} />
+        <Button
+          color="primary"
+          onPress={() => handleButtonUploadExcelPress(apiendpoint)}
+        >
+          Upload Excel
+        </Button>
+      </div>
     </div>
   );
 }
