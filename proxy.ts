f11244bbbeb50b4afc846c31a5731512392d15af
Falconed11 <almacesardavid@gmail.com>
2026-02-11 16:16:02 +0700

    proxy

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,10 @@
-import proxy from "next-auth/middleware";
-
+import { withAuth } from "next-auth/middleware";
+const proxy = withAuth;
 export default proxy;
+
+export const config = {
+  matcher: ["/((?!api/auth|_next/static|_next/image|favicon.ico).*)"],
+};
 
 // import { withAuth, NextRequestWithAuth } from "next-auth/middleware";
 //
