69052995e459920371764792116b7bf63681a692
Falconed11 <almacesardavid@gmail.com>
2026-04-22 11:55:55 +0700

    before change

---
+++ app/(default)/laporan/sample.js
@@ -0,0 +1,34 @@
+const sampleData = {
+  success: true,
+  message: "Success",
+  data: [
+    {
+      id: 1,
+      id_parent: null,
+      nama: "Rugi Laba",
+      level: 0,
+      total_balance: "6000.00",
+    },
+    {
+      id: 2,
+      id_parent: 1,
+      nama: "Gross Provit",
+      level: 1,
+      total_balance: "-6000.00",
+    },
+    {
+      id: 3,
+      id_parent: 2,
+      nama: "Pendapatan",
+      level: 2,
+      total_balance: "-6000.00",
+    },
+    {
+      id: 4,
+      id_parent: 3,
+      nama: "tes",
+      level: 3,
+      total_balance: "6000.00",
+    },
+  ],
+};
