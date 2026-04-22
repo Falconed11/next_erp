05a1886f6a4319a47b66d112bde5ca7d7503bf75
Falconed11 <almacesardavid@gmail.com>
2026-04-22 13:34:31 +0700

    before change

--- app/(default)/laporan/sample.js
+++ app/(default)/laporan/sample.js
@@ -32,3 +32,42 @@ const sampleData = {
     },
   ],
 };
+
+const sampleFullData = {
+  success: true,
+  message: "Success",
+  data: {
+    past: 0,
+    end: 6000,
+    tree: [
+      {
+        id: 1,
+        id_parent: null,
+        nama: "Rugi Laba",
+        level: 0,
+        total_balance: "6000.00",
+      },
+      {
+        id: 2,
+        id_parent: 1,
+        nama: "Gross Provit",
+        level: 1,
+        total_balance: "-6000.00",
+      },
+      {
+        id: 3,
+        id_parent: 2,
+        nama: "Pendapatan",
+        level: 2,
+        total_balance: "-6000.00",
+      },
+      {
+        id: 4,
+        id_parent: 3,
+        nama: "tes",
+        level: 3,
+        total_balance: "6000.00",
+      },
+    ],
+  },
+};
